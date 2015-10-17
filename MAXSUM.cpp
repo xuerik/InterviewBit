e13b3ba2cd791c35944222b33094d4be81b510a3
Erik Xu <xuerik@users.noreply.github.com>
2015-10-16 19:23:20 -0700

    Update MAXSUM.cpp

--- MAXSUM.cpp
+++ MAXSUM.cpp
@@ -1,20 +1,15 @@
-/**
- * @input A : Read only ( DON'T MODIFY ) Integer array
- * @input n1 : Integer array's ( A ) length
- * 
- * @Output Integer
- */
- 
+// Erik Xu
+// C
 // O(n^2) approach
 int maxSubArray(const int* A, int n1) {
     int largest = A[0];
     int total;
     int i, j;
     
-    for(i = 0; i < n1; i++) {
+    for (i = 0; i < n1; i++) {
         total = 0;
         
-        for(j = i; j < n1; j++) {
+        for (j = i; j < n1; j++) {
             total =  total + A[j];
             
             if(total > largest) {
@@ -24,7 +19,47 @@ int maxSubArray(const int* A, int n1) {
     }
     
     return largest;
+}
 
-    
-    
+// C++
+// O(n) approach
+int Solution::maxSubArray(const vector<int> &A) {
+  int currentMax   = 0;
+  int maxSum       = 0;
+  int allNegatives = 0;
+  int actualMax    = -INT_MAX;
+  
+  // Find the minimum element in the vector
+  for (int i = 0; i < A.size(); ++i) {
+    if (A[i] > actualMax) {
+      actualMax = A[i];
+    }
+  }
+  
+  // Flag for whether all input are negatives
+  if (actualMax < 0) {
+    allNegatives = 1;
+  }
+  
+  
+  // Kadane's Algorithm
+  for (int i = 0; i < A.size(); ++i) {
+    if (allNegatives == 0) {
+      currentMax = currentMax + A[i];
+          
+      if (currentMax < 0) {
+        currentMax = 0;
+      } else if (currentMax > maxSum) {
+         maxSum = currentMax;
+      } 
+  }
+      
+    // All negatives
+    else {
+      maxSum = actualMax;
+      break;
+    }
+  }
+  
+  return maxSum;
 }
